1d832ce603003f70df47902a28a8c8956564aad1
zhen <13119626971@163.com>
2018-08-06 22:17:31 +0800

    RecyclerView Adapter

---
+++ app/src/main/java/com/zhen/greendao/adapter/GreendDaoAdapter.java
@@ -0,0 +1,56 @@
+package com.zhen.greendao.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zhen.greendao.R;
+import com.zhen.greendao.entity.Student;
+
+import java.util.List;
+
+public class GreendDaoAdapter extends RecyclerView.Adapter<GreendDaoAdapter.ViewHolder> {
+
+    List<Student> list;
+
+    public GreendDaoAdapter(List<Student> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.student_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Student student = list.get(position);
+        holder.item_id.setText(String.valueOf(student.getId()));
+        holder.item_name.setText(student.getName());
+        holder.item_age.setText(String.valueOf(student.getAge()));
+        holder.item_gender.setText(student.getGender());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        private AppCompatTextView item_id, item_name, item_age, item_gender;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            item_id = itemView.findViewById(R.id.item_id);
+            item_name = itemView.findViewById(R.id.item_name);
+            item_age = itemView.findViewById(R.id.item_age);
+            item_gender = itemView.findViewById(R.id.item_gender);
+        }
+    }
+}
